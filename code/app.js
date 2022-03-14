e20b7efd2fcea504d2c9dcce3dd6b917ecbfeaf6
sama-21 <wasamatsameer@gmail.com>
2022-03-14 10:26:48 +0530

    pfolio_template_bootstrap5

---
+++ code/app.js
@@ -0,0 +1,158 @@
+window.sr = ScrollReveal();
+                    
+sr.reveal('.navbar', {
+  duration: 2000,
+  origin: 'left',
+  distance: '100%' 
+});
+
+sr.reveal('#welcome-right', {
+    duration: 2000,
+    origin: 'right',
+    distance: '100%' 
+});
+
+sr.reveal('#first-welcome', {
+    duration: 4000,
+    origin: 'left',
+    distance: '100%' 
+  });
+
+  sr.reveal('#sec-welcome', {
+    duration: 2000,
+    origin: 'right',
+    distance: '100%' 
+  });  
+
+  sr.reveal('#about-left', {
+    duration: 2000,
+    origin: 'left',
+    distance: '100%',
+    vue: 0.2
+  });
+
+  sr.reveal('#about-think', {
+    duration: 2000,
+    origin: 'right',
+    distance: '40%',
+    vue: 0.2 
+  });
+
+  sr.reveal('#about-right', {
+    duration: 2000,
+    origin: 'right',
+    distance: '40%',
+    vue: 0.2 
+  });
+
+  sr.reveal('#feat-1', {
+    duration: 2000,
+    origin: 'left',
+    distance: '40%',
+    vue: 0.2 
+  });
+
+  sr.reveal('#feat-2', {
+    duration: 2000,
+    origin: 'right',
+    distance: '40%',
+    vue: 0.2 
+  });
+
+  sr.reveal('#feat-3', {
+    duration: 2000,
+    origin: 'bottom',
+    distance: '40%',
+    vue: 0.2 
+  });
+
+  sr.reveal('#work-h5', {
+    duration: 2000,
+    origin: 'left',
+    distance: '40%',
+    vue: 0.2 
+  });
+
+  sr.reveal('#work-h1', {
+    duration: 3000,
+    origin: 'left',
+    distance: '40%',
+    vue: 0.2 
+  });
+
+  sr.reveal('#work-p', {
+    duration: 4000,
+    origin: 'left',
+    distance: '40%',
+    vue: 0.2 
+  });
+
+  sr.reveal('#flex1', {
+    duration: 2000,
+    origin: 'left',
+    distance: '50%',
+    vue: 0.2 
+  });
+
+  sr.reveal('#flex2', {
+    duration: 2000,
+    origin: 'right',
+    distance: '50%',
+    vue: 0.2 
+  });
+
+  sr.reveal('#silicon1', {
+    duration: 2000,
+    origin: 'top',
+    distance: '50%',
+    vue: 0.2 
+  });
+
+  sr.reveal('#silicon2', {
+    duration: 2000,
+    origin: 'bottom',
+    distance: '50%',
+    vue: 0.2 
+  });
+
+  sr.reveal('#folio1', {
+    duration: 2000,
+    origin: 'left',
+    distance: '50%',
+    vue: 0.2 
+  });
+
+  sr.reveal('#folio2', {
+    duration: 2000,
+    origin: 'right',
+    distance: '50%',
+    vue: 0.2 
+  });
+
+  sr.reveal('#btn-more', {
+    duration: 2000,
+    origin: 'bottom',
+    distance: '100%',
+    vue: 0.2 
+  });
+
+  sr.reveal('#contact-left', {
+    duration: 2000,
+    origin: 'left',
+    distance: '50%',
+    vue: 0.2 
+  });
+
+  sr.reveal('#contact-right', {
+    duration: 2000,
+    origin: 'right',
+    distance: '50%',
+    vue: 0.2 
+  });
+
+  sr.reveal('#row-2', {
+    duration: 2000,
+    origin: 'bottom',
+    distance: '100%',
+    vue: 0.2 
+  });
